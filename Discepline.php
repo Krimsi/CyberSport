52440601bde32db307daad34f058c0606f94b85c
Krimsi <astt509@mail.ru>
2019-04-08 16:05:48 +0300

    first

---
+++ Discepline.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Дисцеплины</title>
+
+<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="css/materialize.css" rel="stylesheet">
+<link href="css/materialize.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<link rel="stylesheet" href="css/style.css">
+
+
+
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper  grey darken-4">
+      <a href="#" class="brand-logo" ><img src="logo.jpg" style="padding-left: 50px;"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="Discepline.php">Дисцеплины</a></li>
+        <li><a href="sportsmen.php">Спортсмены</a></li>
+        <li><a href="#">Обратная связь</a></li>
+        <li><a href="comand.php">Команда разработчиков</a></li>
+      </ul>
+    </div>
+  </nav>
+<div class="csgo">
+  
+    <div class="row">
+     <div class="col s2" style="text-align: center;">Турниры CS:GO<img src="img/cs-go-csgo-inventory-icon-27.png"></div>
+      <div class="col s5"> <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/1"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/2"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div></div>
+      <div class="col s4" id="textn">Стартовый этап развития мейджоров начался с DreamHack Winter 2013 и закончился тем же турниром, только в 2014 году. Major-ивенты проходили на небольших площадках, игровое сообщество еще начинало формироваться, а трансляции CS:GO только набирали обороты. Valve продолжила работать с краудфандинговой системой, выпустив после этого еще два кейса — Esports 2013 Winter Case и Esports 2014 Summer Case, но призовой фонд оставался на прежней отметке – $250 000.</div>
+    <div class="col s1"></div>
+    </div>
+          
+
+          <div class="row">
+     <div class="col s2" style="text-align: center;">Турниры Dota 2<img src="img/dota_2_logo_by_flashstep111-daj7nti.png" style="width: 100px;"></div>
+      <div class="col s5"> <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/1"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/2"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div></div>
+      <div class="col s4" id="textn">Стартовый этап развития мейджоров начался с DreamHack Winter 2013 и закончился тем же турниром, только в 2014 году. Major-ивенты проходили на небольших площадках, игровое сообщество еще начинало формироваться, а трансляции CS:GO только набирали обороты. Valve продолжила работать с краудфандинговой системой, выпустив после этого еще два кейса — Esports 2013 Winter Case и Esports 2014 Summer Case, но призовой фонд оставался на прежней отметке – $250 000.</div>
+    <div class="col s1"></div>
+    </div>
+</div>
+  
+
+
+
+
+
+  
+
+
+
+
+
+  <footer class="page-footer grey darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">CyberSport</h5>
+                <p class="white-text ">Что бы чего то добиться, нужно бросить всё и идти к цели. </p>
+                <span>© Саша Simple Костылев</span>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Связь с нами!</h5>
+                <ul>
+                  <li><a class="white-text " href="#!">Мы в VK</a></li>
+                  <li><a class="white-text " href="#!">Мы в Telegram`e</a></li>
+                  <li><a class="white-text " href="#!">Мы в Facebook`е</a></li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Сайт cybersport.Все права защищены.
+            <a class="white-text text-lighten-4 right">Номер для связи +7 (812) 923-13-01</a>
+            </div>
+          </div>
+        </footer>
+</body>
+
+<script src="js/materialize.js"></script>
+<script src="js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script src="js/script.js"></script>
+<script src="js/script2.js"></script>
+</html>
